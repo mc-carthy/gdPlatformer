0d6c7f9b5dca64e360a74592ce9b0e830a3db8c2
Mick McCarthy <mccarthy.mick0012@gmail.com>
2018-04-16 19:38:54 +0100

    Add player acceleration and friction

--- KinematicBody2D.gd
+++ KinematicBody2D.gd
@@ -1,7 +1,8 @@
 extends KinematicBody2D
 
 const UP = Vector2(0, -1)
-const SPEED = 200
+const MAX_SPEED = 200
+const ACC = 50
 const GRAVITY = 20
 const JUMP_SPEED = -500
 
@@ -9,15 +10,15 @@ var motion = Vector2(0, 0)
 
 func _physics_process(delta):
 	if Input.is_action_pressed('ui_right'):
-		motion.x = SPEED
+		motion.x = min(motion.x + ACC, MAX_SPEED)
 		$Sprite.flip_h = false
 		$Sprite.play('run')
 	elif Input.is_action_pressed('ui_left'):
-		motion.x = -SPEED
+		motion.x = max(motion.x - ACC, -MAX_SPEED)
 		$Sprite.flip_h = true
 		$Sprite.play('run')
 	else:
-		motion.x = 0
+		motion.x = lerp(motion.x, 0, 0.2)
 		$Sprite.play('idle')
 	
 	if is_on_floor():
