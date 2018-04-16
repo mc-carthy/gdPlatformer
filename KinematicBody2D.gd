444ee283c5f9c740525138c18fa74addd457e7a5
mc-carthy <mick@mccarthy.tech>
2018-04-16 13:36:26 +0100

    Extract player movement parameters to constants

--- KinematicBody2D.gd
+++ KinematicBody2D.gd
@@ -1,16 +1,19 @@
 extends KinematicBody2D
 
 const UP = Vector2(0, -1)
+const SPEED = 200
+const GRAVITY = 20
+const JUMP_SPEED = -500
 
 var motion = Vector2(0, 0)
 
 func _physics_process(delta):
 	if Input.is_action_pressed('ui_right'):
-		motion.x = 200
+		motion.x = SPEED
 		$Sprite.flip_h = false
 		$Sprite.play('run')
 	elif Input.is_action_pressed('ui_left'):
-		motion.x = -200
+		motion.x = -SPEED
 		$Sprite.flip_h = true
 		$Sprite.play('run')
 	else:
@@ -19,10 +22,10 @@ func _physics_process(delta):
 	
 	if is_on_floor():
 		if Input.is_action_just_released('ui_up'):
-			motion.y = -500
+			motion.y = JUMP_SPEED
 	else:
 		$Sprite.play('jump')
-		motion.y += 20
+		motion.y += GRAVITY
 	move_and_slide(motion, UP)
 	pass
 
