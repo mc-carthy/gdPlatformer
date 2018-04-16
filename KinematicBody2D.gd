b4969ff330dc2df8a5fafada4cba7b84180c6826
mc-carthy <mick@mccarthy.tech>
2018-04-16 13:21:18 +0100

    Create and use animations for player

--- KinematicBody2D.gd
+++ KinematicBody2D.gd
@@ -6,17 +6,23 @@ var motion = Vector2(0, 0)
 
 func _physics_process(delta):
 	if Input.is_action_pressed('ui_right'):
-		motion.x = 100
+		motion.x = 200
+		$Sprite.flip_h = false
+		$Sprite.play('run')
 	elif Input.is_action_pressed('ui_left'):
-		motion.x = -100
+		motion.x = -200
+		$Sprite.flip_h = true
+		$Sprite.play('run')
 	else:
 		motion.x = 0
+		$Sprite.play('idle')
 	
 	if is_on_floor():
 		if Input.is_action_just_released('ui_up'):
-			motion.y = -400
+			motion.y = -500
 	else:
-		motion.y += 10
+		$Sprite.play('jump')
+		motion.y += 20
 	move_and_slide(motion, UP)
 	pass
 
